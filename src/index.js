b89fd3577ffba2e858ba6595f5d8f6961363c242
Gleb Zhigulev <glebzhigulevv@gmail.com>
2024-07-07 23:07:32 +0300

    fix code

--- src/index.js
+++ src/index.js
@@ -6,13 +6,10 @@ import makeFormat from './formatters/index.js'
 const genDiff = (filepath1, filepath2, option = 'stylish') => {
   const data1 = readData(filepath1)
   const data2 = readData(filepath2)
-
   const ext1 = path.extname(filepath1)
   const ext2 = path.extname(filepath2)
-
   const firstData = parseData(data1, ext1)
   const secondData = parseData(data2, ext2)
-
   const objectDiff = generateDiff(firstData, secondData)
   const result = makeFormat(objectDiff, option)
 
